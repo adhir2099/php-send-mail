d3b3443fe64741cff64c5f3e34ce39e09df6100c
Adhir DC <adhir.serrano@gmail.com>
2022-05-07 13:10:38 -0500

    Initial commit

--- sendEmail.php
+++ sendEmail.php
@@ -4,8 +4,8 @@
   $email = $_POST['email'];
   $msg   = $_POST['message'];
 
-  $sender = "dinosandfashion@gmail.com";
-  $subject = "Prueba mail php de hoy 07/05/2022";
+  $sender = "youremail@gmail.com";
+  $subject = "Subject here";
 
   $body = "From: $name \n";
   $body .= "Mail: $email \n";
